b5852a956c5a5ff6d7dabe1a66be59e78f982765
Ishin Iwasaki <ishiniwasaki@gmail.com>
2022-06-20 12:11:08 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const useElectron = process.env.USE_ELECTRON === 'y';
 
 const config = (module.exports = {
 	devtool: 'source-map',
-	mode: isRelease ? 'production' : 'development',
+	mode: 'production',
 	entry: './src/index.js',
 	target: useElectron ? 'electron-renderer' : 'web',
 	output: {
